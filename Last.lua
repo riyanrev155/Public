1e327a7c3e2221c9b9a3270ee6d95a653879404b
Riyan <43004681+riyanrev155@users.noreply.github.com>
2018-09-11 01:38:22 +0700

    Add files via upload

---
+++ Last.lua
@@ -0,0 +1,3 @@
+gg.alert("Script ini sudah berakhir\nUntuk keamanan menjelang Update 0.8")
+
+gg.alert("This script has ENDED. \nPubg will be Update ti v 0.8. for safety i turn off my script.\n\nSorry\n\n\n🇲🇨 Revz Script 🇲🇨")
